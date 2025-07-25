9caf57646befcd0f4ecc3ecc08e1368a01cf235b
Saw Yan Naing <sawyan820@gmail.com>
2025-07-25 11:54:31 +0630

    Finishing more features to come

--- Dota/src/components/Item.jsx
+++ Dota/src/components/Item.jsx
@@ -1,13 +1,43 @@
 import { PiCoinsLight } from "react-icons/pi";
 import { useItemContext } from "./ItemContextProvider";
-
+import { useFloating, offset, flip, shift, autoUpdate } from "@floating-ui/react";
 import {nanoid} from "nanoid"
+import { useEffect, useState } from "react";
 
 export default function Item({name})
 {
+
+    const [open, setOpen] = useState(false)
+    const {refs, floatingStyles} = useFloating({
+        open,
+        onOpenChange : setOpen,
+        middleware: [offset(8), flip(), shift()],
+        placement: "top",
+        whileElementsMounted : autoUpdate
+    })
+
+
     const items = useItemContext()
 
     const item = items[name]
+
+    // update tooltip on scroll to maintain position
+    useEffect(function()
+    {   
+        function closeToolTip()
+        {
+            setOpen(false)
+        }
+
+        if (!open)
+        {
+            return 
+        }
+
+        window.addEventListener("scroll",closeToolTip)
+
+        return window.removeEventListener("scroll", closeToolTip)
+    })
 
     if (!item)
     {
@@ -16,8 +46,15 @@ export default function Item({name})
 
     return(
         <div className="item">
-            <img src={`https://cdn.steamstatic.com${item.img}`} alt={item.dname}/>
-            <div className="item-info">
+            <img 
+                src={`https://cdn.steamstatic.com${item.img}`} 
+                alt={item.dname}
+                ref = {refs.setReference}
+                onClick={() => setOpen(!open)}
+                className={open? "open" : ""}
+            />
+            {open && 
+            <div className="item-info" ref={refs.setFloating} style={floatingStyles}>
                 <h3>{item.dname}</h3>
                 <p className="cost">
                     <PiCoinsLight
@@ -79,6 +116,7 @@ export default function Item({name})
                 }
                 <p className="lore">{item.lore}</p>
             </div>
+            }
         </div>
     )
 }
